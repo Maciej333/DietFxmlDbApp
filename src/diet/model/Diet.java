5bfea04f27a7155a83e44088f476b36b75118321
Maciej Rogowski <maciej.rogowski333@gmail.com>
2020-05-18 17:25:35 +0200

    fill class Diet

--- src/diet/model/Diet.java
+++ src/diet/model/Diet.java
@@ -1,4 +1,45 @@
 package diet.model;
 
+import java.time.LocalDateTime;
+
 public class Diet {
+    private int idDiet;
+    private int idOsoba;
+    private LocalDateTime date;
+
+    public Diet(int idDiet, int idOsoba, LocalDateTime date) {
+        this.idDiet = idDiet;
+        this.idOsoba = idOsoba;
+
+        this.date = date;
+    }
+
+    public int getIdDiet() {
+        return idDiet;
+    }
+
+    public void setIdDiet(int idDiet) {
+        this.idDiet = idDiet;
+    }
+
+    public int getIdOsoba() {
+        return idOsoba;
+    }
+
+    public void setIdOsoba(int idOsoba) {
+        this.idOsoba = idOsoba;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return idDiet+" | "+idOsoba+" | "+date;
+    }
 }
