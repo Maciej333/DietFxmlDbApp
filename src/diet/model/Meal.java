6a4bb06fd0cbfc91a8c41cbc858517db96475a70
Maciej Rogowski <maciej.rogowski333@gmail.com>
2020-05-18 16:49:25 +0200

    fill class Meal

--- src/diet/model/Meal.java
+++ src/diet/model/Meal.java
@@ -1,4 +1,27 @@
 package diet.model;
 
 public class Meal {
+    private int idMeal;
+    private String name;
+
+    public Meal(int idMeal, String name) {
+        this.idMeal = idMeal;
+        this.name = name;
+    }
+
+    public int getIdMeal() {
+        return idMeal;
+    }
+
+    public void setIdMeal(int idMeal) {
+        this.idMeal = idMeal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
