a71026e9d5e8e3a5b6b98c69a3a9cc6fa5808781
Maciej Rogowski <maciej.rogowski333@gmail.com>
2020-05-18 16:48:16 +0200

    fill class Product

--- src/diet/model/Product.java
+++ src/diet/model/Product.java
@@ -1,4 +1,77 @@
 package diet.model;
 
 public class Product {
+    private int idProduct;
+    private String name;
+    private double kcal;
+    private double protein;
+    private double carbs;
+    private double fat;
+    private double fiber;
+
+    public Product(int idProduct, String name, double kcal, double protein, double carbs, double fat, double fiber) {
+        this.idProduct = idProduct;
+        this.name = name;
+        this.kcal = kcal;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fat = fat;
+        this.fiber = fiber;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(double kcal) {
+        this.kcal = kcal;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getFiber() {
+        return fiber;
+    }
+
+    public void setFiber(double fiber) {
+        this.fiber = fiber;
+    }
 }
