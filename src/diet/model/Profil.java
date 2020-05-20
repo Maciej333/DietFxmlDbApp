2855283aa3b43d22519032c1e5365a526e59eb4a
Maciej Rogowski <maciej.rogowski333@gmail.com>
2020-05-20 18:03:14 +0200

    reformate Profil code

--- src/diet/model/Profil.java
+++ src/diet/model/Profil.java
@@ -15,28 +15,33 @@ public class Profil {
     private ProfilSex sex;
     private ProfilGoal goal;
 
-    public Profil(){
+    public Profil() {
 
     }
-    public Profil(int idPerson, String name, int age,  int growth,int weight, String sex, String goal) {
+
+    public Profil(int idPerson, String name, int age, int growth, int weight, String sex, String goal) {
         this.idPerson = idPerson;
         this.name = name;
         this.age = age;
         this.growth = growth;
         this.weight = weight;
-        if(sex.equalsIgnoreCase(ProfilSex.male.toString())){
+        if (sex.equalsIgnoreCase(ProfilSex.male.toString())) {
             this.sex = ProfilSex.male;
-        }else {
+        } else {
             this.sex = ProfilSex.female;
         }
 
-        if(goal.equals(ProfilGoal.loss.toString())){
+        if (goal.equals(ProfilGoal.loss.toString())) {
             this.goal = ProfilGoal.loss;
-        }else if(goal.equals(ProfilGoal.gain.toString())) {
+        } else if (goal.equals(ProfilGoal.gain.toString())) {
             this.goal = ProfilGoal.gain;
-        }else{
+        } else {
             this.goal = ProfilGoal.keep;
         }
+    }
+
+    public static Profil getSelectedProfil() {
+        return selectedProfil;
     }
 
     public int getIdPerson() {
@@ -84,19 +89,16 @@ public class Profil {
     }
 
     public void setSex(String sex) {
-        if(sex.equals(ProfilSex.male.toString())){
+        if (sex.equals(ProfilSex.male.toString())) {
             this.sex = ProfilSex.male;
-        }else {
+        } else {
             this.sex = ProfilSex.female;
-        };
+        }
+        ;
     }
 
     public ProfilGoal getGoal() {
         return goal;
-    }
-
-    public static Profil getSelectedProfil() {
-        return selectedProfil;
     }
 
     public static void setSelectedProfil(Profil selectedProfil) {
@@ -104,31 +106,31 @@ public class Profil {
     }
 
     public void setGoal(String goal) {
-        if(goal.equals(ProfilGoal.loss.toString())){
+        if (goal.equals(ProfilGoal.loss.toString())) {
             this.goal = ProfilGoal.loss;
-        }else if(goal.equals(ProfilGoal.gain.toString())) {
+        } else if (goal.equals(ProfilGoal.gain.toString())) {
             this.goal = ProfilGoal.gain;
-        }else{
+        } else {
             this.goal = ProfilGoal.keep;
         }
     }
 
-    public double countBalans(){
-        double bilans =0;
-        if(sex.equals(ProfilSex.male)){
-            bilans = (9.99*this.weight)+(6.25*this.growth)-(4.92*this.age)+5;
-            if(goal.equals(ProfilGoal.loss)){
-                bilans -=300;
-            }else if(goal.equals(ProfilGoal.gain)){
-                bilans +=300;
+    public double countBalans() {
+        double bilans = 0;
+        if (sex.equals(ProfilSex.male)) {
+            bilans = (9.99 * this.weight) + (6.25 * this.growth) - (4.92 * this.age) + 5;
+            if (goal.equals(ProfilGoal.loss)) {
+                bilans -= 300;
+            } else if (goal.equals(ProfilGoal.gain)) {
+                bilans += 300;
             }
             return bilans;
-        }else{
-            bilans = (9.99*this.weight)+(6.25*this.growth)-(4.92*this.age)-161;
-            if(goal.equals(ProfilGoal.loss)){
-                bilans -=300;
-            }else if(goal.equals(ProfilGoal.gain)){
-                bilans +=300;
+        } else {
+            bilans = (9.99 * this.weight) + (6.25 * this.growth) - (4.92 * this.age) - 161;
+            if (goal.equals(ProfilGoal.loss)) {
+                bilans -= 300;
+            } else if (goal.equals(ProfilGoal.gain)) {
+                bilans += 300;
             }
             return bilans;
         }
