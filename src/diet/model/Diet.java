ba8896b722b9428f4b5a49a7b454645bfe331d59
Maciej Rogowski <maciej.rogowski333@gmail.com>
2020-05-21 02:06:33 +0200

    fill Diet class

--- src/diet/model/Diet.java
+++ src/diet/model/Diet.java
@@ -1,17 +1,32 @@
 package diet.model;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public class Diet {
+
+    private static Diet selectedDiet = null;
+
     private int idDiet;
     private int idOsoba;
     private LocalDateTime date;
+    private Map<Product, Integer> dietProducts;
+    private Map<Meal, Integer> dietMeals;
+    private double kcal;
+    private double protein;
+    private double fat;
+    private double carbs;
+    private double fiber;
 
-    public Diet(int idDiet, int idOsoba, LocalDateTime date) {
-        this.idDiet = idDiet;
-        this.idOsoba = idOsoba;
+    public Diet() {
+    }
 
-        this.date = date;
+    public void setSelectedDiet(Diet diet){
+        selectedDiet = diet;
+    }
+
+    public static Diet getSelectedDiet(){
+        return selectedDiet;
     }
 
     public int getIdDiet() {
@@ -38,8 +53,100 @@ public class Diet {
         this.date = date;
     }
 
-    @Override
-    public String toString() {
-        return idDiet+" | "+idOsoba+" | "+date;
+    public Map<Product, Integer> getDietProducts() {
+        return dietProducts;
+    }
+
+    public void setDietProducts(Map<Product, Integer> dietProducts) {
+        this.dietProducts = dietProducts;
+    }
+
+    public Map<Meal, Integer> getDietMeals() {
+        return dietMeals;
+    }
+
+    public void setDietMeals(Map<Meal, Integer> dietMeals) {
+        this.dietMeals = dietMeals;
+    }
+
+    public double getKcal() {
+        return kcal;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public double getFiber() {
+        return fiber;
+    }
+
+    public void countKcalForDiet(){
+        double kcalForDiet = 0;
+        for(Map.Entry<Product, Integer> productAmount: dietProducts.entrySet()){
+            kcalForDiet += productAmount.getValue()*productAmount.getKey().getKcal()/100;
+        }
+        for(Map.Entry<Meal, Integer> mealAmount: dietMeals.entrySet()){
+            kcalForDiet += mealAmount.getValue()*mealAmount.getKey().getKcal()/100;
+        }
+        this.kcal = Double.parseDouble(roundDouble(kcalForDiet));
+    }
+
+    public void countProteinForDiet(){
+        double proteinForDiet = 0;
+        for(Map.Entry<Product, Integer> productAmount: dietProducts.entrySet()){
+            proteinForDiet += productAmount.getValue()*productAmount.getKey().getProtein()/100;
+        }
+        for(Map.Entry<Meal, Integer> mealAmount: dietMeals.entrySet()){
+            proteinForDiet += mealAmount.getValue()*mealAmount.getKey().getProtein()/100;
+        }
+        this.protein = Double.parseDouble(roundDouble(proteinForDiet));
+    }
+
+    public void countFatForDiet(){
+        double fatForDiet = 0;
+        for(Map.Entry<Product, Integer> productAmount: dietProducts.entrySet()){
+            fatForDiet += productAmount.getValue()*productAmount.getKey().getFat()/100;
+        }
+        for(Map.Entry<Meal, Integer> mealAmount: dietMeals.entrySet()){
+            fatForDiet += mealAmount.getValue()*mealAmount.getKey().getFat()/100;
+        }
+        this.fat = Double.parseDouble(roundDouble(fatForDiet));
+    }
+
+    public void countCarbsForDiet(){
+        double carbsForDiet = 0;
+        for(Map.Entry<Product, Integer> productAmount: dietProducts.entrySet()){
+            carbsForDiet += productAmount.getValue()*productAmount.getKey().getCarbs()/100;
+        }
+        for(Map.Entry<Meal, Integer> mealAmount: dietMeals.entrySet()){
+            carbsForDiet += mealAmount.getValue()*mealAmount.getKey().getCarbs()/100;
+        }
+        this.carbs = Double.parseDouble(roundDouble(carbsForDiet));
+    }
+
+    public void countFiberForDiet(){
+        double fiberForDiet = 0;
+        for(Map.Entry<Product, Integer> productAmount: dietProducts.entrySet()){
+            fiberForDiet += productAmount.getValue()*productAmount.getKey().getFiber()/100;
+        }
+        for(Map.Entry<Meal, Integer> mealAmount: dietMeals.entrySet()){
+            fiberForDiet += mealAmount.getValue()*mealAmount.getKey().getFiber()/100;
+        }
+        this.fiber = Double.parseDouble(roundDouble(fiberForDiet));
+    }
+
+    private String roundDouble(Double doubleToRound){
+        String doubleFormat = String.format("%.2f", doubleToRound);
+        String stringToParse = doubleFormat.replace(",",".");
+        return stringToParse;
     }
 }
